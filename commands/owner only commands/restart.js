61f67e1e5a35a1be2479381e7eea08cf27095722
stip <42151228+stipxx@users.noreply.github.com>
2018-12-01 16:18:34 -0800

    Delete restart.js

--- commands/owner only commands/restart.js
+++
@@ -1,7 +0,0 @@
-if (message.content === `${prefix}restart`) {
-  const token = "something i cant leak xd"
-   if (message.author.id == "310859009479671809")
-       message.react('')
-      message.channel.send('See you next time!')
-       .then(message => client.destroy())
-      client.login(something i cant leak xd)
