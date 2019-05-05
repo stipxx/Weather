800ccde498ee65addd7e8e3543638f8d1497a3d3
stip <42151228+stipxx@users.noreply.github.com>
2019-05-04 20:08:15 -0700

    Delete atlanticsatellite.js

--- src/commands/atlanticsatellite.js
+++
@@ -1,7 +0,0 @@
-if (message.content === `${prefix}atlanticsatellite`) {
-  const embed = new Discord.RichEmbed()
-  .setTitle('Atlantic Ocean Satellite from the Weather Center/NHC:')
-  .setColor('62e55c')
-  .setDescription('https://weather.com/maps/satellite/atlanticoceanweathermap')
-  message.channel.send(embed);
- message.react("?");
