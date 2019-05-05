2998c566a621bce9351dcf124e9a00cd42376312
stip <42151228+stipxx@users.noreply.github.com>
2019-05-04 20:08:57 -0700

    Delete pacificsatellite.js

--- src/commands/pacificsatellite.js
+++
@@ -1,7 +0,0 @@
-if (message.content === `${prefix}pacificsatellite`) {
-const embed = new Discord.RichEmbed()
- .setTitle('Pacific Ocean Satellite from the Weather Center/NHC:')
- .setColor('62e55c')
- .setDescription('https://weather.com/maps/satellite/pacificoceanweathermap')
-message.channel.send(embed);
-message.react("?");
